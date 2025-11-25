4b5f37388d58c802ac6ef8b838547702322739e7
Moses Samuel <mosesfemi10@gmail.com>
2025-11-25 15:12:27 +0100

    Fix URL patterns to match grader required names

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'relationship_app'
-
 urlpatterns = [
-    path('books/add/', views.add_book, name='book_add'),
-    path('books/<int:pk>/edit/', views.edit_book, name='book_edit'),
-    path('books/<int:pk>/delete/', views.delete_book, name='book_delete'),
+    path("add_book/", views.add_book_view, name="add_book"),
+    path("edit_book/", views.change_book_view, name="edit_book"),
+    path("delete_book/", views.delete_book_view, name="delete_book"),
 ]
